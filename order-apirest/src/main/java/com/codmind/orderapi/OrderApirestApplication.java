177f30f92fc91de6a99e2b2d68512618c3b5db5d
David Ramirez <davramir@redhat.com>
2021-07-18 19:39:36 -0500

    Primera version de api-rest

---
+++ order-apirest/src/main/java/com/codmind/orderapi/OrderApirestApplication.java
@@ -0,0 +1,13 @@
+package com.codmind.orderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApirestApplication.class, args);
+	}
+
+}
